79bba78b23ff438d641653a5667e83093dedf15a
cheng <chengl@bu.edu>
2022-07-21 14:04:47 -0400

    add logic for completing calculation

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const display = document.querySelector('.display');
 const operands = document.querySelectorAll('.operand');
 const operators = document.querySelectorAll('.operator');
+const equalsBtn = document.querySelector('#equals');
 
 operands.forEach((operand) => {
        operand.addEventListener('click', () => {
+            if(hasOperator){
+                display.textContent = '';
+                hasOperator = false;
+            }
             display.textContent += operand.innerHTML;
        });
 });
@@ -11,5 +16,51 @@ operands.forEach((operand) => {
 const allClear = document.querySelector('#ac');
 allClear.addEventListener('click', () => {
     display.innerHTML = '';
+    prevNum = ''
+    currOperation = '';
+    curNum = '';
 });
 
+let currOperation = '';
+let prevNum ='';
+let curNum = '';
+let hasOperator = false;
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        prevNum = display.innerHTML;
+        currOperation = operator.innerHTML;
+        hasOperator = true;
+        console.log(currOperation);
+        console.log(prevNum);
+    })
+});
+
+ equalsBtn.addEventListener('click', () => {
+    curNum = display.innerHTML;
+    result = calcuate(prevNum, curNum, currOperation);
+    display.innerHTML = result;
+    prevNum = result;
+ });
+
+function calcuate(prevNum, curNum, operator){
+    prevNum = parseInt(prevNum);
+    curNum = parseInt(curNum);
+    if(operator == '+'){
+        return prevNum + curNum;
+    }
+    if(operator == '-'){
+        return prevNum - curNum;
+    }
+    if(operator == '*'){
+        return prevNum * curNum;
+    }
+    if(operator == '/'){
+        return prevNum / curNum;
+    }
+    if(operator == '%'){
+        return 'IDK HOW THIS WORKS';
+    }
+    if(operator == '+/-'){
+        return curNum * -1;
+    }
+}
